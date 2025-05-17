5877f40672bf2b5902383a5daf2ff27f3185c9b3
HKienDev <caythuemw1@gmail.com>
2025-05-17 20:44:20 +0700

    Update API

--- sport-store/src/middlewares/rateLimit.js
+++ sport-store/src/middlewares/rateLimit.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 phút
-  max: 10, // tối đa 10 lần/15 phút cho mỗi IP
+  max: 1000, // tăng giới hạn lên 1000 lần/15 phút cho mỗi IP để tránh lỗi 429 khi test
   message: {
     success: false,
     message: 'Bạn đã thử đăng nhập quá nhiều lần. Vui lòng thử lại sau 15 phút.',
